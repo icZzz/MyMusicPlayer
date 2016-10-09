12b475125c0251713d11289dd9b3806a20e31210
赵传龙 <zhaochuanlong@huan.tv>
2016-10-09 11:19:56 +0800

    create music view

---
+++ Player/Player/MusicCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MusicCollectionViewCell.swift
+//  Player
+//
+//  Created by 趙傳龍 on 2016/10/8.
+//  Copyright © 2016年 Huan. All rights reserved.
+//
+
+import UIKit
+
+class MusicCollectionViewCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        
+    }
+}
---
+++ Player/Player/MusicListCollectionViewController.swift
@@ -0,0 +1,90 @@
+//
+//  MusicListCollectionViewController.swift
+//  Player
+//
+//  Created by 趙傳龍 on 2016/10/8.
+//  Copyright © 2016年 Huan. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "ioCell"
+
+class MusicListCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+//        self.collectionView!.register(MusicCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 30
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MusicCollectionViewCell
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("sss \(indexPath.row)")
+    }
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
